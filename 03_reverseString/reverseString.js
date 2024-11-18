f48722d484eb8cbddf27455a8d754f72f680ffb4
ElvisDaniel1 <Elvisdabiedeen5@gmail.com>
2024-11-18 05:15:51 -0400

    Complete js-exercise reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(message) {
+    let new_message = "";
+    
+    msg_length = message.length - 1;
 
+    for (let i = msg_length; i >= 0; i--)
+    {
+        new_message += message[i];
+    }
+
+    return new_message;
 };
 
 // Do not edit below this line
