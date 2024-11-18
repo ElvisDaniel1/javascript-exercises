12297efcf1567ed2ae5fe64f3f95b64f428dd60c
ElvisDaniel1 <Elvisdabiedeen5@gmail.com>
2024-11-18 07:14:11 -0400

    Complete JS-exercise tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  return Number(((fahren - 32) * 5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celc) {
+  return Number((celc * (9/5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
