1f6a2ca51b907f8a1206f47fa848ed93a45c1406
ElvisDaniel1 <elvisdabiedeen5@gmail.com>
2025-03-12 11:08:53 -0400

    Complete JS palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
 
+const palindromes = function (word) {
+
+    word = word.toLowerCase();
+    word = word.split('');
+    word = word.filter((wrd) => {
+        if (!['!', '?', ',', '.', ' '].includes(wrd))
+            return wrd;
+        });
+    word = word.join('');
+    let wordR = '';
+
+    const wordLen = word.length;
+    let i = wordLen;
+    while (i != 0) {
+        wordR += word[i-1];
+        i = i - 1;   
+    }
+
+    if (wordR == word)
+        return true;
+        //console.log('YES ITS PALINDROME');
+    else
+        return false;
+        //console.log('No its not');
+        //racecar
 };
 
 // Do not edit below this line
