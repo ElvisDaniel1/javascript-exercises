61a44255c80c60e5d0775877db46e3059c214f25
ElvisDaniel1 <elvisdabiedeen5@gmail.com>
2025-03-12 12:16:53 -0400

    Complete JS fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (typeof(n) == "string")
+        n = parseInt(n);
+     
+    function fib(n){
+        if (n == 1)
+            return 1;
+        if (n == 0)
+            return 0;
+        if (n < 0)
+            return 'OOPS';
+        return fib(n-1) + fib(n-2);
+    }
+
+    return fib(n);
 };
 
 // Do not edit below this line
