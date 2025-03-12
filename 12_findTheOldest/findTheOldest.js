b74e5584ff29062718655f2115b5fa0ac4172eac
ElvisDaniel1 <elvisdabiedeen5@gmail.com>
2025-03-12 13:31:49 -0400

    Complete JS findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+let ages = [];
+
+for (person in people) {
+    if (people[person]['yearOfDeath'] == undefined)
+        people[person]['yearOfDeath'] = 2024;
+    let n = (people[person]['yearOfDeath'] - people[person]['yearOfBirth']);
+    ages.push(n);
+}
+
+let agesLength = ages.length;
+let largest = 0;
+let position = 0;
+
+for (let i = 0; i < agesLength; i++){
+    if (ages[i] > largest){
+        largest = ages[i];
+        position = i;
+    }
+}
+
+return( people[position]);
 
 };
 
